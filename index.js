2a60c40aa8617f3aa7986887af086d512be01e26
Franu16 <torresfrancesca194@gmail.com>
2025-08-21 16:38:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Bienvenido a nuestra calculadora")
+//creamos las variables para la SUMA
+let numeroUNO= prompt("Introduce el primer número: ")//sring
+let numeroDOS= prompt("Introduce el segundo número: ")
+//convertimos en númeroslos datos 
+numeroUNO=parseInt(numeroUNO)
+numeroDOS=parseInt(numeroDOS)
+//probamos si capturamos los datos 
+console.log("Los números que ingresaron son " + númeroUNO +"y" + númeroDOS);
+//creamos la calculadora 
+console.log(numeroUNO+numeroDOS);
